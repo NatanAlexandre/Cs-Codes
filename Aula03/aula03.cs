699eace9e1f30cbbe72af790df40eed6c8d281fc
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-18 22:18:21 -0300

    Update aula03.cs

--- Aula03/aula03.cs
+++ Aula03/aula03.cs
@@ -4,6 +4,8 @@ class Main {
     static void Main(string[] args){
         int num = 0;
         char letra = 'c';
-        float valor = 5.3f; 
+        float valor = 5.3f;
+        byte n1 = 10 // 0 e 255
+        string nome = "Natan";
     }
 }

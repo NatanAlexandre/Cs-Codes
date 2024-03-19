15ee2e51aa6811fdfaece42d285f6bd0259bfa99
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-18 22:28:23 -0300

    Update

---
+++ Aula03/cont.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Principal {
+    static void Main(string[] args){
+        int num1, num2, res;
+
+        num1 = 10;
+        num2 = 2;
+        res = num1 * num2;
+
+        Console.WriteLine("A multiplicação de " + num1 + " Vezes " + num2 + " É igual a: " + res);
+    }
+}

f7d1e8f0cea2b7ff111ce2505aae07c2bc03fe3b
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-18 22:40:21 -0300

    Update aula04.cs

--- Aula04/aula04.cs
+++ Aula04/aula04.cs
@@ -2,12 +2,10 @@ using System;
 
 class Program {
 
-    int num = 10;
+    static int num = 10;
     static void Main(string[] args){
         int num2 = 0;
-    }
-
-    void teste(){
         Console.WriteLine(num);
     }
+
 }

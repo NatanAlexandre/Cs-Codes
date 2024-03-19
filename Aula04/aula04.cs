0b5652b3f6c8c2d1f24cf65e66a334733b41bb73
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-18 22:34:08 -0300

    Update

---
+++ Aula04/aula04.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+
+    int num = 10;
+    static void Main(string[] args){
+        int num2 = 0;
+    }
+
+    void teste(){
+        Console.WriteLine(num);
+    }
+}

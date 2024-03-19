6c95945c9d43b28d1ba3170321853fc9a1efac61
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-18 22:09:00 -0300

    Update

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -4,6 +4,9 @@ namespace Aula02 {
     class Program {
         static void Main(string[] args){
             Console.WriteLine("Hello World!");
+            if (args.GetLength(0) > 0){
+                Console.Write(args.GetValue(0));
+            }
         }
     }
 }

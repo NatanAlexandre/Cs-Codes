efd15847309cfff17dde340b0607d21aede83be2
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-18 23:25:51 -0300

    Update

--- Aula06/aula06.cs
+++ Aula06/aula06.cs
@@ -6,6 +6,6 @@ class Program {
 
         n1 = 10; n2 = 20; n3 = 30;
 
-        Console.WriteLine("n1={0}, n2={1}, n3={2}", n1, n2, n3);
+        Console.WriteLine("\nn1\t=\t{0}\nn2\t=\t{1}\nn3\t=\t{2}", n1, n2, n3);
     }
 }

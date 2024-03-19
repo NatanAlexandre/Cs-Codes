04337dd28a60b42600fe59ba9282eacb26ec0bab
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-18 22:24:11 -0300

    Update

--- Aula03/aula03.cs
+++ Aula03/aula03.cs
@@ -1,13 +1,15 @@
 using System;
 
-class Main {
+class Program {
     static void Main(string[] args){
         int num = 0;
         char letra = 'c';
         float valor = 5.3f;
-        byte n1 = 10 // 0 e 255
+        byte n1 = 10; // 0 e 255
         string nome = "Natan";
 
-        var aux = 10; // Var não recebe tipo da variável
+        var aux = nome; // Var não recebe tipo da variável
+
+        Console.WriteLine("Valor da variável: " + aux + "...");
     }
 }

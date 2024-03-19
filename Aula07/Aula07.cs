00ebdf754d6e603dd52f481f2d40a18a1dc570a8
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-19 16:06:32 -0300

    Update

--- Aula07/Aula07.cs
+++ Aula07/Aula07.cs
@@ -2,6 +2,9 @@ using System;
 
 class Program {
     static void Main(string[] args){
-        
+        const string canal = "CFB Cursos";
+        const double pi = 3.14;
+
+        Console.WriteLine("Canal: {0}", canal);
     }
 }

781548e1ea33e3b3dab0ec5d54309aca8b3fae49
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-20 08:19:25 -0300

    Update

--- Aula12/code.cs
+++ Aula12/code.cs
@@ -2,6 +2,13 @@ using System;
 
 class Program {
     static void Main(string[] args){
-        
+        int nota;
+        string resultado = "Reprovado";
+
+        if(nota > 60){
+            resultado = "Aprovado";
+        }
+
+        Console.WriteLine("Resultado: {0}", resultado);
     }
 }

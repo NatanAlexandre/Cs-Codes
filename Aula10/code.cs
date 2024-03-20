7518c97ef93eb19f6ccb34503f87970009df5722
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-19 23:51:24 -0300

    Update

--- Aula10/code.cs
+++ Aula10/code.cs
@@ -4,9 +4,9 @@ class Program {
     enum DiasSemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado};
 
     static void Main(string[] args) {
-        DiasSemana ds = (int)DiasSemana.Sexta;
-        DiasSemana ds2 = (DiasSemana)0;
+        int ds = (int)DiasSemana.Sexta;
+        DiasSemana ds2 = (DiasSemana)3;
 
-        Console.WriteLine("{0}", ds);
+        Console.WriteLine("{0}, {1}", ds, ds2);
     }
 }

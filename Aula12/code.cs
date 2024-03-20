3696f7276c120bf5b71d544447124840f80a4b91
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-20 08:56:14 -0300

    Update

--- Aula12/code.cs
+++ Aula12/code.cs
@@ -2,13 +2,27 @@ using System;
 
 class Program {
     static void Main(string[] args){
-        int nota;
-        string resultado = "Reprovado";
+        float n1, n2, n3, n4, media;
 
-        if(nota > 60){
-            resultado = "Aprovado";
+        Console.Write("Digite a primeira nota: ");
+        n1 = float.Parse(Console.ReadLine());
+        Console.Write("Digite a segunda nota: ");
+        n2 = float.Parse(Console.ReadLine());
+        Console.Write("Digite a terceira nota: ");
+        n3 = float.Parse(Console.ReadLine());
+        Console.Write("Digite a quarta nota: ");
+        n4 = float.Parse(Console.ReadLine());
+
+        media = ((n1 + n2 + n3 + n4) / 4f);
+
+        if(media >= 6f){
+            Console.WriteLine("Parabéns, você foi aprovado!");
+        } else if (media >= 4f) {
+            Console.WriteLine("Você ficou de recuperação!");
+        } else {
+            Console.WriteLine("Você foi reprovado!");
         }
 
-        Console.WriteLine("Resultado: {0}", resultado);
+        Console.WriteLine("Media: {0}", media);
     }
 }

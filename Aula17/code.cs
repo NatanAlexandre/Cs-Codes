894d44023be1facbf8a8f4722f3938f948a15f09
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-20 14:43:14 -0300

    Update

--- Aula17/code.cs
+++ Aula17/code.cs
@@ -1,22 +1,23 @@
 using System;
 
-// Array
-
+// Vetores e Matrizes
 class Program {
     static void Main(string[] args){
-        int n1, n2, n3, n4, n5;
+        int[,] n = new int[3, 5];
+        int[,] num = new int[2, 2]{{10, 20}, {30, 40}};
 
-        int[] n = new int[5]; // 0
-        int[] num = new int[3]{55, 77, 99};
-        int[] numl = {55, 77, 99}; // usa o número de elemntos passados
+        /*
+        10 20 30 40 50 
+        60 70 80 90 15
+        25 35 45 55 65
+        */
+        
 
-        n[0] = 11;
-        n[1] = 22;
-        n[2] = 33;
-        n[3] = 44;
-        n[4] = 55;
+        n[0, 0] = 10; n[0, 1] = 20; n[0, 2] = 30; n[0, 3] = 40; n[0, 4] = 50; 
+        n[1, 0] = 60; n[1, 1] = 70; n[1, 2] = 80; n[1, 3] = 90; n[1, 4] = 15;
+        n[2, 0] = 25; n[2, 1] = 35; n[2, 2] = 45; n[2, 3] = 55; n[2, 4] = 65;
 
-        Console.WriteLine(n[4]);
-        Console.WriteLine(num[2]);
+        Console.WriteLine(n[1, 3]);
+        Console.WriteLine(n[2, 1]);
     }
 }

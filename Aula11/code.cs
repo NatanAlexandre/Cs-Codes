d898d7451c9e1af5efe82749bc327326d7e000ed
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-20 00:02:02 -0300

    Update

--- Aula11/code.cs
+++ Aula11/code.cs
@@ -2,6 +2,10 @@ using System;
 
 class Program {
     static void Main(string[] args){
-        
+        // Typecast
+        int vInt = 10;
+        short vShort = (short)vInt;
+
+        Console.WriteLine(vShort);
     }
 }

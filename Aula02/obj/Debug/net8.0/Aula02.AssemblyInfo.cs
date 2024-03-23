3ee5b68cfb5ad448765916709cd908fea095b45e
Natan Alexandre <111938020+NatanAlexandre@users.noreply.github.com>
2024-03-22 22:27:51 -0300

    Update

--- Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d898d7451c9e1af5efe82749bc327326d7e000ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+894d44023be1facbf8a8f4722f3938f948a15f09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
